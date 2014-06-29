f1af8029b6d8ef7f3d8a7cd9b01c0aa449da8da6
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-06-29 11:12:33 -0400

    ...

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ object Main {
 
     new FindANewApartment(Config("http://www.kijiji.ca",
       "/b-appartement-condo/ville-de-montreal/villeray/k0c37l1700281?origin=ps",
-      30 second))
+      500 milliseconds))
 
   }
 
